70e5b38d065a5f06980cd8bb5ce46624f374ecd7
rodrigob <rodrigob@mnmaster>
2016-02-21 11:01:33 -0300

    Funcionando21/02/2016

--- src/validatenacluster/ValidateNacluster.java
+++ src/validatenacluster/ValidateNacluster.java
@@ -9,57 +9,56 @@ package validatenacluster;
  *
  * @author rodrigob
  */
-import java.net.URI;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.*;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
 
 public class ValidateNacluster {
-        String configurationHdfs;
-    public static void main(String[] args) throws Exception {
 
-        if (args.length != 2) {
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
-            System.err.println("Usage: ValidateNacluster <input path/folder> <output path/folder>");
-            System.exit(-1);
+        private final static IntWritable one = new IntWritable(1);
+        InetAddress ip;
+        
+        private Text word = new Text("Total Lines");
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            
+            ip = InetAddress.getLocalHost();       
+        
+            Text word = new Text(ip.getHostName());
+            output.collect(word, one);
         }
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Partition for Machine Count");
-        
-        job.setJarByClass(ValidateNacluster.class);
-        job.setJobName("Partition for Machine Count");
-        
-        /*String configurationHdfs = "";
-        Configuration conf;
-        conf = new Configuration();
+    }
 
-        URI uri = new URI(configurationHdfs);
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
 
-        FileSystem hdfs = FileSystem.get(uri, conf);
-   
-        // Delete old folder in the parameter two
-        Path newFolderPath = new Path(args[1]);
-       
-        if (hdfs.exists(newFolderPath)) {
-            hdfs.delete(newFolderPath, true); //Delete existing Directory
-        }*/
-        
-     FileInputFormat.addInputPaths(job, args[0]);
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        
-        job.setMapperClass(CountMapper.class);
-        
-        job.setReducerClass(CountReducer.class);
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
+        }
+    }
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    public static void main(String[] args) throws Exception {
+        
+        JobConf conf = new JobConf(ValidateNacluster.class);
+        conf.setJobName("Partition for Machine Count");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+        conf.setReducerClass(Reduce.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+        JobClient.runJob(conf);
+        
     }
 }
